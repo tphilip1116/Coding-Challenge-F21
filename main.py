d774a246638987973cd7151608e48b88c84b0f12
tphilip1116 <38962450+tphilip1116@users.noreply.github.com>
2021-08-26 11:05:54 -0500

    updating the main solution

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 with open('input.txt','r') as f:
     lines = f.readlines()
 
+#Using an instance of the VADER Sentiment Analyzer
 sia = SentimentIntensityAnalyzer()
 
+#Tracking the amoung of line in file
 lineNum = 0
+#Calculating the sentiment value for each line
 sentimentScore = 0
+
 for x in lines:
     lineNum = lineNum + 1
-    print(lineNum, ": ", sia.polarity_scores(x).get('compound'))
-    sentimentScore += sia.polarity_scores(x).get('compound')
+    print(lineNum, ": ", sia.polarity_scores(x).get('compound')) #Printing each individul score for the line
+    sentimentScore += sia.polarity_scores(x).get('compound') #accumulating the scores for the file
 
-sentimentScore /= lineNum
+#Determining the average score for entire file
+sentimentScore /= lineNum 
+
+#Displaying the calculated total score
 print("Overall Sentiment Score: ", sentimentScore)
 
 
